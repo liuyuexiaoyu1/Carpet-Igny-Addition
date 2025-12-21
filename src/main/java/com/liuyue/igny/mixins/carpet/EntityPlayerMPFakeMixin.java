57320f4022896128dbcb2b2a058c866e42ff108f
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2025-12-22 00:59:41 +0800

    优化宝库假人使用离线档案逻辑

--- src/main/java/com/liuyue/igny/mixins/carpet/EntityPlayerMPFakeMixin.java
+++ src/main/java/com/liuyue/igny/mixins/carpet/EntityPlayerMPFakeMixin.java
@@ -11,17 +11,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.mojang.authlib.GameProfile;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+//#if MC >= 12109
+//$$ import java.util.UUID;
+//#endif
+
 @Mixin(EntityPlayerMPFake.class)
 public abstract class EntityPlayerMPFakeMixin {
     //#if MC >= 12003
+    //#if MC >= 12109
+    //$$ @ModifyVariable(method = "createFake", at = @At("STORE"), name = "uuid")
+    //$$ private static UUID modifyGameProfile(UUID value, @Local(argsOnly = true, name = "arg0") String username) {
+    //#else
     @ModifyVariable(method = "createFake", at = @At("STORE"), name = "gameprofile")
-    private static GameProfile modifyGameProfile(GameProfile profile, @Local(argsOnly = true, name = "arg0") String username) {
+    private static GameProfile modifyGameProfile(GameProfile value, @Local(argsOnly = true, name = "arg0") String username) {
+    //#endif
         for (ITask task : TaskManager.getAllActiveTasks()) {
             if (task instanceof VaultTask vaultTask && username.equals(vaultTask.getPendingFakeName())) {
+                //#if MC >= 12109
+                //$$ return UUIDUtil.createOfflinePlayerUUID(username);
+                //#else
                 return new GameProfile(UUIDUtil.createOfflinePlayerUUID(username), username);
+                //#endif
             }
         }
-        return profile;
+        return value;
     }
     //#endif
 }
