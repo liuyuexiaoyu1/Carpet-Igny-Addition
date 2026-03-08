083d1f4ddd3d86a6a2e84437070cc00f9878c538
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-08 23:07:24 +0800

    🐛修复：注入失败

--- src/main/java/com/liuyue/igny/mixins/rule/structureBlockNoBlockUpdate/StructureBlockEntityMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/structureBlockNoBlockUpdate/StructureBlockEntityMixin.java
@@ -3,9 +3,7 @@ package com.liuyue.igny.mixins.rule.structureBlockNoBlockUpdate;
 import com.liuyue.igny.IGNYSettings;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.entity.StructureBlockEntity;
@@ -13,22 +11,9 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlac
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StructureBlockEntity.class)
 public class StructureBlockEntityMixin {
-    //#if MC <= 12002
-    //$$ @Inject(method = "loadStructure(Lnet/minecraft/server/level/ServerLevel;ZLnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Lnet/minecraft/util/RandomSource;I)Z"))
-    //#else
-    @Inject(method = "placeStructure(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Lnet/minecraft/util/RandomSource;I)Z"))
-    //#endif
-    private void placeStructure(ServerLevel serverLevel, StructureTemplate structureTemplate, CallbackInfo ci, @Local StructurePlaceSettings structurePlaceSettings) {
-        if (IGNYSettings.structureBlockNoBlockUpdate) {
-            structurePlaceSettings.setKnownShape(true);
-        }
-    }
-
     //#if MC <= 12002
     //$$ @WrapOperation(method = "loadStructure(Lnet/minecraft/server/level/ServerLevel;ZLnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Lnet/minecraft/util/RandomSource;I)Z"))
     //#else
@@ -36,7 +21,8 @@ public class StructureBlockEntityMixin {
     //#endif
     private boolean placeStructure_noBlockUpdateFlag(StructureTemplate instance, ServerLevelAccessor serverLevelAccessor, BlockPos blockPos, BlockPos blockPos2, StructurePlaceSettings structurePlaceSettings, RandomSource randomSource, int i, Operation<Boolean> original) {
         if (IGNYSettings.structureBlockNoBlockUpdate) {
-           return original.call(instance, serverLevelAccessor, blockPos, blockPos2, structurePlaceSettings, randomSource, 2 | 16);
+            structurePlaceSettings.setKnownShape(true);
+            return original.call(instance, serverLevelAccessor, blockPos, blockPos2, structurePlaceSettings, randomSource, 2 | 16);
         }
         return original.call(instance, serverLevelAccessor, blockPos, blockPos2, structurePlaceSettings, randomSource, i);
     }
