7b3028be8a12b4e68d2fb2f2ede672fc60c46218
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-23 03:28:10 +0800

    🐛修复：编译失败

---
+++ versions/26.1/src/main/java/com/liuyue/igny/utils/deobfuscator/mapping/TinyMappingV2Reader.java
@@ -0,0 +1,17 @@
+package com.liuyue.igny.utils.deobfuscator.mapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Copy from Carpet-TIS-Addition
+ * 此文件以LGPL-3.0协议开源
+ */
+
+public class TinyMappingV2Reader implements MappingReader {
+    @Override
+    public Map<String, String> readMapping(BufferedReader mappingReader) throws IOException {
+        return Map.of();
+    }
+}
