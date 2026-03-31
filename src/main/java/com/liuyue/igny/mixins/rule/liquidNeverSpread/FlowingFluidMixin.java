04bde7f0b9213ac5805fa02794a239ebf79863f9
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-31 23:32:39 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/mixins/rule/liquidNeverSpread/FlowingFluidMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/liquidNeverSpread/FlowingFluidMixin.java
@@ -9,11 +9,25 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//#if MC >= 12102
+//$$ import net.minecraft.world.level.block.state.BlockState;
+//$$ import net.minecraft.server.level.ServerLevel;
+//#endif
 
 @Mixin(FlowingFluid.class)
 public class FlowingFluidMixin {
     @Inject(method = "spread", at = @At(value = "HEAD"), cancellable = true)
-    private void spread(Level level, BlockPos pos, FluidState state, CallbackInfo ci) {
+    private void spread(
+            //#if MC >= 12102
+            //$$ ServerLevel level,
+            //#else
+            Level level,
+            //#endif
+            BlockPos pos,
+            //#if MC >= 12102
+            //$$ BlockState blockState,
+            //#endif
+            FluidState state, CallbackInfo ci) {
         if (!IGNYSettings.liquidNeverSpread.equals("false")) {
             if (IGNYSettings.liquidNeverSpread.equals("true") || state.isSource()) {
                 ci.cancel();
--- src/main/java/com/liuyue/igny/mixins/rule/liquidNeverSpread/LiquidBlockMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/liquidNeverSpread/LiquidBlockMixin.java
@@ -5,20 +5,13 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FlowingFluid;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LiquidBlock.class)
 public class LiquidBlockMixin {
-    @Shadow
-    @Final
-    protected FlowingFluid fluid;
-
     @Inject(method = "shouldSpreadLiquid", at = @At(value = "HEAD"), cancellable = true)
     private void shouldSpreadLiquid(Level level, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
         if (!IGNYSettings.liquidNeverSpread.equals("false")) {
