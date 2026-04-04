1a2f0d332f6a96023a3f29369775ae80439c8474
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-04-05 01:46:52 +0800

    🐛修复：注入失败

--- src/main/java/com/liuyue/igny/mixins/rule/superEffectLevel/MobEffectInstanceMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/superEffectLevel/MobEffectInstanceMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MobEffectInstance.class)
 public class MobEffectInstanceMixin {
+    //#if MC >= 12005
     @WrapOperation(method = "<init>(Lnet/minecraft/core/Holder;IIZZZLnet/minecraft/world/effect/MobEffectInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(III)I"))
     private int init(int value, int min, int max, Operation<Integer> original, @Local(ordinal = 1, argsOnly = true) int amplifier) {
         if (IGNYSettings.superEffectLevel) {
@@ -17,4 +18,5 @@ public class MobEffectInstanceMixin {
         }
         return original.call(value, min, max);
     }
+    //#endif
 }
