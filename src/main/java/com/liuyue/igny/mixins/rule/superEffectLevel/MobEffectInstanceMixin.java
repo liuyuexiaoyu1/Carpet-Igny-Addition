7574c968c1b4ee6b208ff12dd1a7eeb19a86324e
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-04-05 01:24:03 +0800

    🐛修复：实际得到的效果等级与预期不符的问题

---
+++ src/main/java/com/liuyue/igny/mixins/rule/superEffectLevel/MobEffectInstanceMixin.java
@@ -0,0 +1,20 @@
+package com.liuyue.igny.mixins.rule.superEffectLevel;
+
+import com.liuyue.igny.IGNYSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.effect.MobEffectInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MobEffectInstance.class)
+public class MobEffectInstanceMixin {
+    @WrapOperation(method = "<init>(Lnet/minecraft/core/Holder;IIZZZLnet/minecraft/world/effect/MobEffectInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(III)I"))
+    private int init(int value, int min, int max, Operation<Integer> original, @Local(ordinal = 1, argsOnly = true) int amplifier) {
+        if (IGNYSettings.superEffectLevel) {
+            return amplifier;
+        }
+        return original.call(value, min, max);
+    }
+}
