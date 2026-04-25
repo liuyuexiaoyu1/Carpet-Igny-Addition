efa943d63b2aefb25a72d93eedb82db708491e29
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-04-26 01:21:24 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/IGNYSettings.java
+++ src/main/java/com/liuyue/igny/IGNYSettings.java
@@ -2,10 +2,8 @@ package com.liuyue.igny;
 
 import carpet.api.settings.Rule;
 import com.liuyue.igny.rule.annotation.ObservedRule;
-import com.liuyue.igny.rule.callback.EffectLevelCallback;
-import com.liuyue.igny.rule.callback.GameTickCallback;
-import com.liuyue.igny.rule.validators.EntityValidator;
-import com.liuyue.igny.rule.validators.SyncmaticaValidator;
+import com.liuyue.igny.rule.callback.*;
+import com.liuyue.igny.rule.validators.*;
 import net.minecraft.core.BlockPos;
 
 import java.util.*;
