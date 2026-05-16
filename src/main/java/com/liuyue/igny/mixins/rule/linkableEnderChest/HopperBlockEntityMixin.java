323a7db40ae64f482929687eed5763caac0aaf2e
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-05-16 16:36:51 +0800

    🐛修复：漏斗上方存在末影箱时无法吸取世界中的物品实体

---
+++ src/main/java/com/liuyue/igny/mixins/rule/linkableEnderChest/HopperBlockEntityMixin.java
@@ -0,0 +1,41 @@
+package com.liuyue.igny.mixins.rule.linkableEnderChest;
+
+import com.liuyue.igny.IGNYSettings;
+import com.liuyue.igny.utils.interfaces.linkableEnderChest.LinkedEnderChest;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.Hopper;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
+
+@Mixin(value = HopperBlockEntity.class, priority = 1001)
+public class HopperBlockEntityMixin {
+    @ModifyVariable(method = "suckInItems", at = @At(value = "STORE"))
+    private static Container getSourceContainer(Container value, @Local BlockState blockState, @Local BlockPos blockPos, @Local(argsOnly = true) Level level, @Local(argsOnly = true) Hopper hopper) {
+        if (blockState.is(Blocks.ENDER_CHEST)) {
+            if (!IGNYSettings.linkableEnderChest) {
+                return null;
+            }
+            if (level.getBlockEntity(blockPos) instanceof LinkedEnderChest enderChest) {
+                if (enderChest.carpet_Igny_Addition$isLinked()) {
+                    List<ItemEntity> items = HopperBlockEntity.getItemsAtAndAbove(level, hopper);
+                    if (!items.isEmpty()) {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            }
+        }
+        return value;
+    }
+}
