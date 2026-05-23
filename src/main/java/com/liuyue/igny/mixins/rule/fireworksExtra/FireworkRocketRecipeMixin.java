685bfbde65eb8a6c068152a162c32781f4c90276
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-05-23 12:25:35 +0800

    🐛修复：1.20.6-游戏在合成烟花时崩溃的问题

--- src/main/java/com/liuyue/igny/mixins/rule/fireworksExtra/FireworkRocketRecipeMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/fireworksExtra/FireworkRocketRecipeMixin.java
@@ -9,7 +9,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(FireworkRocketRecipe.class)
 public class FireworkRocketRecipeMixin {
     @ModifyConstant(
+            //#if MC >= 12101
             method = "matches(Lnet/minecraft/world/item/crafting/CraftingInput;Lnet/minecraft/world/level/Level;)Z",
+            //#else
+            //$$ method = "matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z",
+            //#endif
             constant = @Constant(intValue = 3)
     )
     private int extendMaxGunpowder(int original) {
