4922f1fdabcf56d1e9eb0fab08c1a2410cdd6ace
fcsailboat <3286637847@qq.com>
2026-05-24 13:27:37 +0800

    重写隐形物品展示框

--- src/main/java/com/liuyue/igny/mixins/rule/invisibleItemFrames/ItemFrameMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/invisibleItemFrames/ItemFrameMixin.java
@@ -1,81 +1,32 @@
 package com.liuyue.igny.mixins.rule.invisibleItemFrames;
 
 import com.liuyue.igny.IGNYSettings;
-import com.liuyue.igny.utils.interfaces.invisibleItemFrames.ItemFrameRefreshable;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+//#if MC < 12108
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.decoration.ItemFrame;
-import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//#if MC >= 12106
-//$$ import net.minecraft.world.level.storage.ValueInput;
+//#else
 //$$ import net.minecraft.world.level.storage.ValueOutput;
 //#endif
+import net.minecraft.world.entity.decoration.ItemFrame;
+import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ItemFrame.class)
-public class ItemFrameMixin implements ItemFrameRefreshable {
-    @Unique
-    private boolean igny$modManagedInvisible = false;
-
-    @Inject(method = "setItem(Lnet/minecraft/world/item/ItemStack;Z)V", at = @At(value = "RETURN"))
-    private void onSetItem(ItemStack stack, boolean updateNeighbours, CallbackInfo ci) {
-        igny$refreshInvisible(IGNYSettings.INVISIBLE_ITEM_FRAMES.value());
-    }
-
-    @Inject(method = "readAdditionalSaveData", at = @At("RETURN"))
-    //#if MC >= 12106
-    //$$ private void onReadNbt(ValueInput tag, CallbackInfo ci)
-    //#else
-    private void onReadNbt(CompoundTag tag, CallbackInfo ci)
-    //#endif
-    {
-        if (tag.contains("IgnyInvisibleItemFrame")) {
-            //#if MC >= 12105
-            //$$ igny$modManagedInvisible = tag.getBooleanOr("IgnyInvisibleItemFrame", false);
+public class ItemFrameMixin {
+    @WrapMethod(method = "addAdditionalSaveData")
+    private void saving(
+            //#if MC < 12108
+            CompoundTag value,
             //#else
-            igny$modManagedInvisible = tag.getBoolean("IgnyInvisibleItemFrame");
+            //$$ ValueOutput value,
             //#endif
-        }
-        igny$refreshInvisible(IGNYSettings.INVISIBLE_ITEM_FRAMES.value());
-    }
-
-    @Inject(method = "addAdditionalSaveData", at = @At("RETURN"))
-    //#if MC >= 12106
-    //$$ private void onWriteNbt(ValueOutput tag, CallbackInfo ci)
-    //#else
-    private void onWriteNbt(CompoundTag tag, CallbackInfo ci)
-    //#endif
-    {
-        if (igny$modManagedInvisible) {
-            tag.putBoolean("IgnyInvisibleItemFrame", true);
-        }
-    }
-
-    @Override
-    public void igny$refreshInvisible(String ruleValue) {
-        ItemFrame self = (ItemFrame) (Object) this;
-        if (!self.hasCustomName()) return;
-
-        boolean shouldHide;
-        if ("false".equalsIgnoreCase(ruleValue)) {
-            shouldHide = false;
-        } else {
-            String frameName = self.getName().getString();
-            boolean nameMatches = "true".equalsIgnoreCase(ruleValue)
-                    ? "invisible".equalsIgnoreCase(frameName)
-                    : frameName.equalsIgnoreCase(ruleValue);
-            shouldHide = nameMatches && !self.getItem().isEmpty();
-        }
-
-        if (shouldHide) {
-            self.setInvisible(true);
-            igny$modManagedInvisible = true;
-        } else if (igny$modManagedInvisible) {
-            self.setInvisible(false);
-            igny$modManagedInvisible = false;
+            Operation<Void> original
+    ) {
+        try {
+            IGNYSettings.ITEM_FRAME_SAVE_NBT.set(true);
+            original.call(value);
+        } finally {
+            IGNYSettings.ITEM_FRAME_SAVE_NBT.set(false);
         }
     }
 }
--- src/main/java/com/liuyue/igny/utils/interfaces/invisibleItemFrames/ItemFrameRefreshable.java
+++
@@ -1,5 +0,0 @@
-package com.liuyue.igny.utils.interfaces.invisibleItemFrames;
-
-public interface ItemFrameRefreshable {
-    void igny$refreshInvisible(String ruleValue);
-}
