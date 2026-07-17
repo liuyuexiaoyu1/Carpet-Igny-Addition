51d2355241858af9ee032e2b5b099ae5ad7ad1b8
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-07-17 13:49:14 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/mixins/rule/lightQueueBlockedCanLoadLightedChunk/ChunkMapInvoker.java
+++ src/main/java/com/liuyue/igny/mixins/rule/lightQueueBlockedCanLoadLightedChunk/ChunkMapInvoker.java
@@ -2,7 +2,7 @@ package com.liuyue.igny.mixins.rule.lightQueueBlockedCanLoadLightedChunk;
 
 import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ChunkMap;
-//#if MC <= 12001
+//#if MC <= 12004
 //$$ import net.minecraft.world.level.ChunkPos;
 //#endif
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ChunkMap.class)
 public interface ChunkMapInvoker {
-    //#if MC <= 12001
+    //#if MC <= 12004
     //$$ @Invoker("releaseLightTicket")
     //$$ void invokeReleaseLightTicket(ChunkPos chunkPos);
     //#else
