0600c554f38b55001c609a5d7c657c96d5b15299
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-07-29 15:02:21 +0800

    🐛修复：破坏方块踢出玩家

--- versions/1.21.10/src/main/java/com/liuyue/igny/mixins/rule/copperGolemDropOnlyNormal/BlockMixin.java
+++ versions/1.21.10/src/main/java/com/liuyue/igny/mixins/rule/copperGolemDropOnlyNormal/BlockMixin.java
@@ -2,6 +2,7 @@ package com.liuyue.igny.mixins.rule.copperGolemDropOnlyNormal;
 
 import com.liuyue.igny.IGNYSettings;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CopperGolemStatueBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +17,7 @@ public class BlockMixin {
     @ModifyVariable(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;", at = @At(value = "HEAD"), argsOnly = true)
     //#endif
     private static BlockState getDrops(BlockState state) {
-        if (IGNYSettings.COPPER_GOLEM_DROP_ONLY_NORMAL.value()) {
+        if (state.is(Blocks.COPPER_GOLEM_STATUE) && IGNYSettings.COPPER_GOLEM_DROP_ONLY_NORMAL.value()) {
             return state.setValue(CopperGolemStatueBlock.POSE, CopperGolemStatueBlock.Pose.STANDING);
         }
         return state;
