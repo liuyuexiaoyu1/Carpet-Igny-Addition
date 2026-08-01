49946caf3ddcc11b46cfcd0a8c33bf903418e600
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-08-01 14:28:34 +0800

    🐛修复：注入失败

--- src/main/java/com/liuyue/igny/mixins/rule/respawnBlockNeverExplode/RespawnAnchorBlockMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/respawnBlockNeverExplode/RespawnAnchorBlockMixin.java
@@ -9,20 +9,20 @@ import net.minecraft.world.level.block.RespawnAnchorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-//#if MC >= 12102
+//#if MC >= 12111
 //$$ import net.minecraft.server.level.ServerLevel;
 //#endif
 
 @Mixin(RespawnAnchorBlock.class)
 public class RespawnAnchorBlockMixin {
-    //#if MC >= 12102
+    //#if MC >= 12111
     //$$ @WrapOperation(method = "useWithoutItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/RespawnAnchorBlock;explode(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)V"))
     //#elseif MC <= 12004
     //$$ @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/RespawnAnchorBlock;explode(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"))
     //#else
     @WrapOperation(method = "useWithoutItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/RespawnAnchorBlock;explode(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"))
     //#endif
-    //#if MC >= 12102
+    //#if MC >= 12111
     //$$ private void explode(RespawnAnchorBlock instance, BlockState state, ServerLevel level, BlockPos pos2, Operation<Void> original)
     //#else
     private void explode(RespawnAnchorBlock instance, BlockState state, Level level, BlockPos pos2, Operation<Void> original)
