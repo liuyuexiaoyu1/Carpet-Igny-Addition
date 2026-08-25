5dec1faa6ce7e8d9b066ad091edb1cc5f5c54747
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-08-25 18:22:22 +0800

    🐛修复：注入失败

--- src/main/java/com/liuyue/igny/mixins/rule/wifiBeacon/BeaconBlockEntityAccessor.java
+++ src/main/java/com/liuyue/igny/mixins/rule/wifiBeacon/BeaconBlockEntityAccessor.java
@@ -1,6 +1,8 @@
 package com.liuyue.igny.mixins.rule.wifiBeacon;
 
+//#if MC >= 12005
 import net.minecraft.core.Holder;
+//#endif
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.level.block.entity.BeaconBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,14 +17,30 @@ public interface BeaconBlockEntityAccessor {
     void igny$setLevels(int levels);
 
     @Accessor("primaryPower")
+        //#if MC <= 12004
+        //$$ MobEffect igny$getPrimaryPower();
+        //#else
     Holder<MobEffect> igny$getPrimaryPower();
+    //#endif
 
     @Accessor("primaryPower")
+        //#if MC <= 12004
+        //$$ void igny$setPrimaryPower(MobEffect primaryPower);
+        //#else
     void igny$setPrimaryPower(Holder<MobEffect> primaryPower);
+    //#endif
 
     @Accessor("secondaryPower")
+        //#if MC <= 12004
+        //$$ MobEffect igny$getSecondaryPower();
+        //#else
     Holder<MobEffect> igny$getSecondaryPower();
+    //#endif
 
     @Accessor("secondaryPower")
+        //#if MC <= 12004
+        //$$ void igny$setSecondaryPower(MobEffect primaryPower);
+        //#else
     void igny$setSecondaryPower(Holder<MobEffect> secondaryPower);
+    //#endif
 }
