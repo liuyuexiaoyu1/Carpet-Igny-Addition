134cd69fad5d60a76765b6364bbd3b67ceac9712
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-08-30 02:16:19 +0800

    🐛修复：水流可推动鱿鱼 (waterCanPushSquid)规则无效

---
+++ src/main/java/com/liuyue/igny/mixins/rule/waterCanPushSquid/WaterAnimalMixin.java
@@ -0,0 +1,32 @@
+package com.liuyue.igny.mixins.rule.waterCanPushSquid;
+
+import com.liuyue.igny.IGNYSettings;
+import net.minecraft.world.entity.animal.Squid;
+//#if MC >= 12103
+//$$ import net.minecraft.world.entity.animal.AgeableWaterCreature;
+//#else
+import net.minecraft.world.entity.animal.WaterAnimal;
+//#endif
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+//#if MC >= 12103
+//$$ @Mixin(AgeableWaterCreature.class)
+//#else
+@Mixin(WaterAnimal.class)
+//#endif
+public class WaterAnimalMixin {
+    @Inject(method = "isPushedByFluid", at = @At(value = "HEAD"), cancellable = true)
+    private void isPushedByFluid(CallbackInfoReturnable<Boolean> cir) {
+        //#if MC >= 12103
+        //$$ AgeableWaterCreature waterAnimal = (AgeableWaterCreature) (Object) this;
+        //#else
+        WaterAnimal waterAnimal = (WaterAnimal) (Object) this;
+        //3endif
+        if (IGNYSettings.WATER_CAN_PUSH_SQUID.value() && waterAnimal instanceof Squid) {
+            cir.setReturnValue(true);
+        }
+    }
+}
